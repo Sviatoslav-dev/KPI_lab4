d65da6218c1ee697b734c582241f588e81186436
yasha424 <jakob20022404@gmail.com>
2021-04-08 11:52:24 +0300

    added student functional

--- main.cpp
+++ main.cpp
@@ -1,61 +1,191 @@
+#include "student.h"
 #include<iostream>
 #include<fstream>
 #include<vector>
 
 using namespace std;
 
-class User {
-public:
-    string name;
-    string password;
-    string type;
-};
 
-void input_users (vector<User> &users) {
-    ifstream users_file;
-    string line;
+void write_student(vector <Student> &students){
 
-    users_file.open("Users.csv");
-    while (!users_file.eof()) {
-        getline(users_file, line);
-        User u;
-        for (int i = 0, k = 0; i < line.size(); i++) {
-            if (line[i] != ',') {
-                if (k == 0) {
-                    u.name += line[i];
-                } else if (k == 1) {
-                    u.password += line[i];
-                } else if (k == 2) {
-                    u.type += line[i];
-                }
-            } else {
-                k++;
-            }
-        }
-        users.push_back(u);
+  ifstream file("Users.csv");
+  Student student;
+
+  string str;
+  while (!file.eof()) {
+    file >> str;
+    // cout << str;
+    student.setName(str.substr(0, str.size()-1));
+
+    file >> str;
+    student.setPassword(str.substr(0, str.size()-1));
+
+    file >> str;
+    student.setGroup(str.substr(0, str.size()-1));
+
+    for (size_t i = 0; i < 6; i++) {
+      file >> str;
+      // cout << str;
+      student.setSubjects(str.substr(0, str.size()-1), i);
+      str = "";
+
+      file >> str;
+      // cout << str;
+      student.setMarks(stoi(str.substr(0, str.size()-1)), i);
+      str = "";
+      // cout << student.getSubjects(i) << "," << student.getMarks(i) << endl;
     }
+
+    for (size_t i = 0; i < 3; i++) {
+      file >> str;
+      student.setSession(str.substr(0, str.size()-1), i);
+
+      file >> str;
+      student.setSessionMarks(stoi(str.substr(0, str.size()-1)), i);
+
+      // cout << student.getSession(i) << " " << student.getSessionMarks(i) << endl;
+    }
+    // cout << student.getName() << endl << student.getPassword() << endl;
+
+    students.push_back(student);
+  }
 }
 
-User login () {
-    string log, pass;
-    vector<User> users;
+void print_info(vector <Student> students){
+  for (size_t i = 0; i < students.size(); i++) {
+    cout << students[i].getName() << endl;
 
-    while (true) {
-        cout << "Input login:";
-        getline(cin, log);
-        cout << "Input password:";
-        getline(cin, pass);
+    cout << students[i].getPassword() << endl;
 
-        for (int i = 0; i < users.size(); i++) {
-            if (log == users[i].name && pass == users[i].password) {
-                return users[i];
-            }
-        }
+    cout << students[i].getGroup() << endl;
 
-        cout << "Wrong login or password" << endl;
+    for (size_t j = 0; j < 6; j++) {
+      cout << students[i].getSubjects(j) << ",";
+
+      cout << students[i].getMarks(j) << endl;
+
     }
+
+    for (size_t j = 0; j < 3; j++) {
+      cout << students[i].getSession(j) << ",";
+
+      cout << students[i].getSessionMarks(j) << endl;
+
+      // cout << student.getSession(i) << " " << student.getSessionMarks(i) << endl;
+    }
+
+  }
 }
 
-int main () {
-    login ();
+void make_file(vector <Student> students){
+  ofstream file("Users.csv");
+  for (size_t i = 0; i < students.size(); i++) {
+
+    file << students[i].getName() << ", ";
+
+    file << students[i].getPassword() << ", ";
+
+    file << students[i].getGroup() << ", ";
+
+    for (size_t j = 0; j < 6; j++) {
+      file << students[i].getSubjects(j) << ", ";
+
+      file << students[i].getMarks(j) << ", ";
+
+    }
+
+    for (size_t j = 0; j < 3; j++) {
+      if (j < 2) {
+        file << students[i].getSession(j) << ", ";
+
+        file << students[i].getSessionMarks(j) << ", ";
+      }
+      else{
+        file << students[i].getSession(j) << ", ";
+
+        file << students[i].getSessionMarks(j)<< ",";
+      }
+
+      // cout << student.getSession(i) << " " << student.getSessionMarks(i) << endl;
+    }
+    if (i < students.size()-1) {
+      file << endl;
+    }
+
+  }
+}
+
+
+bool find_user(string login, string password, vector <Student> students, int &ind){
+  bool ok = false;
+  for (size_t i = 0; i < students.size(); i++) {
+    if (login == students[i].getName() && password == students[i].getPassword()) {
+      ok = true;
+      ind = i;
+    }
+  }
+  return ok;
+}
+
+void print_marks(Student student){
+  for (size_t i = 0; i < 6; i++) {
+    cout << student.getSubjects(i) << ": " << student.getMarks(i) << endl;
+  }
+}
+
+void print_sessionMarks(Student student){
+  for (size_t i = 0; i < 3; i++) {
+    cout << student.getSession(i) << ": " << setw (10) << student.getSessionMarks(i) << endl;
+  }
+}
+
+int main(){
+  string decPassword = "password";
+  string decLogin = "decanat";
+  vector <Student> students;
+  write_student(students);
+  string login, password;
+  string str;
+  int ind, diya;
+  do {
+    cout << "Введіть логін: "; cin >> login;
+    cout << "Введіть пароль: "; cin >> password;
+    if (login == decLogin && password == decPassword) {
+      cout << "ви деканат" << endl;
+    }
+
+    else if (find_user(login, password, students, ind)) {
+      do {
+        cout << "ви студент!" << endl;
+        // cout << ind << endl;
+        cout << "Що ви хочете зробити?\n 1) переглянути оцінки\n 2) переглянути результати сесії\n 3) змінити пароль\n 4) вийти\n";
+        cout << "Введіть цифру-номер дії: "; cin >> diya;
+        if (diya == 1) {
+          print_marks(students[ind]);
+        }
+        else if (diya == 2) {
+          print_sessionMarks(students[ind]);
+        }
+
+        else if (diya == 3) {
+          string pass;
+            cout << "будь ласка, введіть новий пароль: ";
+            cin >> pass;
+            students[ind].setPassword(pass);
+
+        }
+
+        else if (diya == 4) {
+          str = "exit";
+        }
+      } while(diya != 4);
+    }
+
+    else{
+        cout << "ви ніхто!" << endl;
+    }
+    make_file(students);
+
+  } while(str != "exit");
+  // print_info(students);
 }
---
+++ student.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student{
+private:
+  string name;
+  string password;
+  string group;
+  string subjects[6];
+  int marks[6];
+  string session[3];
+  int sessionMarks[3];
+
+public:
+  Student(){ }
+
+  void setName(string str){
+    name = str;
+  }
+
+  string getName(){
+    return name;
+  }
+
+  void setPassword(string str){
+    password = str;
+  }
+
+  string getPassword(){
+    return password;
+  }
+
+  void setGroup(string str){
+    group = str;
+  }
+
+  string getGroup(){
+    return group;
+  }
+
+  void setSubjects(string str, int i){
+    subjects[i] = str;
+  }
+
+  string getSubjects(int i){
+    return subjects[i];
+  }
+
+  void setMarks(int mark, int i){
+    marks[i] = mark;
+  }
+
+  int getMarks(int i){
+    return marks[i];
+  }
+
+  void setSession(string str, int i){
+    session[i] = str;
+  }
+
+  string getSession(int i){
+    return session[i];
+  }
+
+  void setSessionMarks(int mark, int i){
+    marks[i] = mark;
+  }
+
+  int getSessionMarks(int i){
+    return marks[i];
+  }
+
+};
