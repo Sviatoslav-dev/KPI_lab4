01647b5a0b5274ff0ae103d419de9ba2d8392227
yasha424 <jakob20022404@gmail.com>
2021-04-21 16:12:04 +0300

    added login window

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    private Button SignInButton;
+
+    @FXML
+    private TextField LoginField;
+
+    @FXML
+    void initialize() {
+        SignInButton.setOnAction(event -> {
+            System.out.println("ok");
+        });
+    }
 }
