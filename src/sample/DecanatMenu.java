1ddbca0afd2f1e495fcc2ee80cd7088d91d2f219
yasha424 <jakob20022404@gmail.com>
2021-04-21 20:17:37 +0300

    added exit button for decanat

--- src/sample/DecanatMenu.java
+++ src/sample/DecanatMenu.java
@@ -1,11 +1,17 @@
 package sample;
 
-import javafx.fxml.FXML;
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class DecanatMenu {
+
     @FXML
     private ResourceBundle resources;
 
@@ -13,7 +19,30 @@ public class DecanatMenu {
     private URL location;
 
     @FXML
-    void initialize() {
+    private Button Exit;
 
+    @FXML
+    private Button Session;
+
+    @FXML
+    private Button LoginUser;
+
+    @FXML
+    void initialize() {
+        Exit.setOnAction(event -> {
+            Exit.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("sample.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
     }
 }
