45bd19d97b8f75816fa428cd8a32b1f62b508827
yasha424 <jakob20022404@gmail.com>
2021-04-22 14:15:46 +0300

    added marks window

--- src/sample/ShowMarksStudent.java
+++ src/sample/ShowMarksStudent.java
@@ -1,14 +1,13 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class ShowMarksStudent {
+
     @FXML
     private ResourceBundle resources;
 
@@ -16,10 +15,36 @@ public class ShowMarksStudent {
     private URL location;
 
     @FXML
-    ScrollPane SP;
+    private Label marks;
+
+    @FXML
+    private Label sum;
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private Label showMarks;
 
     @FXML
     void initialize() {
+        String m = "";
+
+        for (int i = 0; i < Main.db.students.size(); i++) {
+            m += Main.db.students.get(i).name + " - ";
+            for (int j = 0; j < Main.db.students.get(i).subjects.size(); j++) {
+                if (Main.db.students.get(i).subjects.get(j).name.equals(Main.subject)) {
+                    if (Main.db.students.get(i).subjects.get(j).subject_marks != null) {
+                        for (int p = 0; p < Main.db.students.get(i).subjects.get(j).subject_marks.size(); p++) {
+                            m += Main.db.students.get(i).subjects.get(j).subject_marks.get(p).toString() + ", ";
+                        }
+                    }
+                }
+            }
+            m += "\n";
+        }
+
+        marks.setText(m);
 
     }
 }
